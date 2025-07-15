03196a37b25b9b6c0cc2006d4efb8d15d71e64ba
Hasan <209364140+Hasan292228@users.noreply.github.com>
2025-07-15 21:26:03 +0500

    uh fixed somethings

--- pong.py
+++ pong.py
@@ -28,83 +28,80 @@ def start_game():
 
 def move_ball():
     global x_velocity, y_velocity
-    
-    x_current = ball.winfo_x()
-    y_current = ball.winfo_y()
-    left_paddle_x = left_paddle.winfo_x() + left_paddle.winfo_width()
-    left_paddle_y = left_paddle.winfo_y() - 10
-    right_paddle_x = right_paddle.winfo_x()
-    right_paddle_y = right_paddle.winfo_y() - 10    
+
+    ball.place_configure(x=ball.winfo_x() + x_velocity, y=ball.winfo_y() + y_velocity)
 
     # for y axis
-    if y_current <= 0:
+    if ball.winfo_y() <= 0:
         y_velocity = -y_velocity
-        ball.place_configure(x=x_current + x_velocity, y=y_current + y_velocity)
-    elif y_current + 25 >= 450:
+        ball.place_configure(x=ball.winfo_x() + x_velocity, y=ball.winfo_y() + 10)
+        return
+    if ball.winfo_y() + 25 >= 450:
         y_velocity = -y_velocity
-        ball.place_configure(x=x_current, y=415)
-    
+        ball.place_configure(x=ball.winfo_x(), y=ball.winfo_y() - 10)
+        return
+
     # for x axis
-    if x_current <= left_paddle_x and y_current >= left_paddle_y and y_current <= left_paddle_y + 110:
-        x_velocity = -x_velocity
+    if ball.winfo_x() <= left_paddle.winfo_x() + 15 and ball.winfo_y() >= left_paddle.winfo_y() - 5 and ball.winfo_y() <= left_paddle.winfo_y() + 105:
         inc_speed()
-        ball.place_configure(x=ball.winfo_x() + x_velocity, y=ball.winfo_y() + y_velocity)
-    elif x_current + 25 >= right_paddle_x and y_current >= right_paddle_y and y_current <= right_paddle_y + 110:
         x_velocity = -x_velocity
+        ball.place_configure(x=ball.winfo_x() + 10, y=ball.winfo_y())
+        return
+    if ball.winfo_x() + 25 >= right_paddle.winfo_x() - 5 and ball.winfo_y() >= right_paddle.winfo_y() - 5 and ball.winfo_y() <= right_paddle.winfo_y() + 105:
         inc_speed()
-        ball.place_configure(x=ball.winfo_x() + x_velocity, y=ball.winfo_y() + y_velocity)
+        x_velocity = -x_velocity
+        ball.place_configure(x=ball.winfo_x() - 10, y=ball.winfo_y())
+        return
 
     # ball not touching any paddle
-    elif x_current < 20:
+    if ball.winfo_x() < 15:
         right_score.config(text=int(right_score.cget("text")) + 1)
         initialize()
-    elif x_current > 555:
+        return
+    if ball.winfo_x() + 25 > 585:
         left_score.config(text=int(left_score.cget("text")) + 1)
         initialize()
-    else:
-        ball.place_configure(x=ball.winfo_x() + x_velocity, y=ball.winfo_y() + y_velocity)
+        return
 
 def move_paddles():
     global gameStarted
     if not gameStarted:
         return
     if "w" in keys_pressed:
-        if left_paddle.winfo_y() > 5:
+        if left_paddle.winfo_y() > 15:
             left_paddle.place_configure(y=left_paddle.winfo_y() - 15)
     if "s" in keys_pressed:
-        if left_paddle.winfo_y() < 345:
+        if left_paddle.winfo_y() < 335:
             left_paddle.place_configure(y=left_paddle.winfo_y() + 15)
     if "Up" in keys_pressed:
-        if right_paddle.winfo_y() > 5:
+        if right_paddle.winfo_y() > 15:
             right_paddle.place_configure(y=right_paddle.winfo_y() - 15)
     if "Down" in keys_pressed:
-        if right_paddle.winfo_y() < 345:
+        if right_paddle.winfo_y() < 335:
             right_paddle.place_configure(y=right_paddle.winfo_y() + 15)
 
 def initialize():
     global x_velocity, y_velocity
 
-    ball.place_configure(x=287.5, y=225)
+    ball.place_configure(x=288, y=225)
 
-    x_velocity = randint(2,3) * randint(-1,1)
-    while x_velocity == 0:
-        x_velocity = randint(2,3) * randint(-1,1)
-    y_velocity = randint(2,3) * randint(-1,1)
-    while y_velocity == 0:
-        y_velocity = randint(2,3) * randint(-1,1)
+    randomint = randint(0, 1)
+    x_velocity = randint(1, 3) if randomint == 0 else -randint(1, 3)
+    randomint = randint(0, 1)
+    y_velocity = randint(1, 3) if randomint == 0 else -randint(1, 3)
 
 def inc_speed():
     global x_velocity, y_velocity
-    if x_velocity < 10:
+    if x_velocity > -10 and x_velocity < 10:
         if x_velocity > 0:
-            x_velocity += 0.5
+            x_velocity += 1
         else:
-            x_velocity -= 0.5
-    if y_velocity < 10:
+            x_velocity -= 1
+    if y_velocity > -10 and y_velocity < 10:
         if y_velocity > 0:
-            y_velocity += 0.5
+            y_velocity += 1
         else:
-            y_velocity -= 0.5
+            y_velocity -= 1
 
 def game_loop():
     move_paddles()
@@ -163,7 +160,7 @@ right_score.place(x=400, y=20)
 
 # create ball
 ball = tk.Canvas(game_frame, width=25, height=25, bg="black", highlightthickness=0)
-ball.place(x=287.5, y=225)
+ball.place(x=288, y=225)
 drawn_ball = ball.create_oval(0, 0, 25, 25, fill="white")
 
 # start the game
